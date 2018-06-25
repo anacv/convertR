5fb6bfd1e2728abedab01287ff9d2377d48ef73c
jbedia <joaquin.bedia@gmail.com>
2018-06-25 15:01:47 +0200

    Add new built-in example datasets (NCEP reanalysis)

---
+++ R/rdas.R
@@ -0,0 +1,34 @@
+#' @title Surface temperature gridded dataset (var = \code{"tas"})
+#' @name tas.iberia
+#' @template templateDatasets
+NULL
+
+#' @title Specific humidity gridded dataset (var = \code{"huss"})
+#' @name huss.iberia
+#' @template templateDatasets
+NULL
+
+#' @title Surface pressure gridded dataset (var = \code{"ps"})
+#' @name ps.iberia
+#' @template templateDatasets
+NULL
+
+#' @title Longwave incoming solar radiation gridded dataset (var = \code{"rlds"})
+#' @name rlds.iberia
+#' @template templateDatasets
+NULL
+
+#' @title Shortwave incoming solar radiation gridded dataset (var = \code{"rsds"})
+#' @name rsds.iberia
+#' @template templateDatasets
+NULL
+
+#' @title Surface geopotential height gridded dataset (var = \code{"zgs"})
+#' @name zgs.iberia
+#' @note This variable is static (i.e., constant along time), and therefore all the time-related arguments
+#' indicated in the \code{loadGridData} call are ignored
+#' @template templateDatasets
+NULL
+
+
+
