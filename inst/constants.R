eb987fcbfce7b41b9806d5f57e8c07f91fec8904
jbedia <joaquin.bedia@gmail.com>
2018-06-25 17:29:38 +0200

    Add constants file

---
+++ inst/constants.R
@@ -0,0 +1,6 @@
+Rv <- 461.5 # J/(K kg)
+Rd <- 287.058 # dry air constant J/(K kg)
+epsilon <- Rd/Rv # adimensional
+T0 <- 273.15 # K
+es0 <- 611 # Pa
+GammaST <- .0065 # (dT/dz)^st standard atmosphere vertical gradient of the temperature in the troposphere (0.0065 (K/m^-1))
